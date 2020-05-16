b28314ad58efeb76bc74cf74d0d04272c9762d79
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 14:22:36 -0300

    Exercicio 14

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio14 {
+    class Program {
+        static void Main(string[] args) {
+            int D = 4;
+            string Dia = "";
+
+            if (D == 1) Dia = "Domingo";
+            else if (D == 2) Dia = "Segunda";
+
+            if (D == 3) Dia = "Terca";
+            else if (D == 4) Dia = "Quarta";
+
+            if (D == 5) Dia = "Quinta";
+
+            if (D == 6) Dia = "Sexta";
+            else Dia = "Sabado";
+            Console.WriteLine("Hoje e {0}", Dia);
+        }
+    }
+}
