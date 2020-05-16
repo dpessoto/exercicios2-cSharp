c0a2784a30e21446273ae5f27cb2fc3eb916f2b4
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 10:29:50 -0300

    Exercicio 1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio1 {
+
+    public class Aluno {
+        public string NomeAluno { get; set; }
+
+        public Aluno() {
+            this.NomeAluno = "";
+        }
+        public Aluno(string Nome) {
+            NomeAluno = Nome;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno aluno = new Aluno();
+
+            Aluno aluno2 = new Aluno("Daniel");
+        }
+    }
+}
