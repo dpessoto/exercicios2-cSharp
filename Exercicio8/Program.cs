641257b522b6573b2b6894309ff2fe37cab8baf4
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 12:42:33 -0300

    Exercicio 8

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio8 {
+    public class Pessoa {
+        protected string _TipoDocumento;
+        public string Nome { get; set; }
+        protected string NumeroDocumento { get; set; }
+        public string TipoDocumento { get { return _TipoDocumento; } }
+    }
+    public class PessoaJuridica : Pessoa {
+        //ValidaCNPJ valCnpj = new ValidaCNPJ(); //Não tem classe ValidaCNPJ
+        public string CNPJ {
+            set { this.NumeroDocumento = value; }
+            get { return this.NumeroDocumento; }
+        }
+        public PessoaJuridica() { _TipoDocumento = "CNPJ"; }
+        public bool CnpjValido() {
+            return false;
+           //valCnpj.IsCnpj(this.NumeroDocumento);
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
