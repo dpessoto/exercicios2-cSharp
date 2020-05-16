63afdf31760d24261f8af7553ac7b7f18a45fb13
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 13:53:43 -0300

    Exercicio 12

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12 {
+    class Program {
+
+        static void Main(string[] args) {
+            int z = 0;
+            while (z < 10) {
+                Console.WriteLine("Primeiro LOOP: " + z);
+                string v = z.ToString();
+                z++;
+            }
+            z = 0;
+            while (z > 3) {
+                Console.WriteLine("Segundo LOOP: " + z);
+                string v = z.ToString();
+                z++;
+            }
+        }
+    }
+}
