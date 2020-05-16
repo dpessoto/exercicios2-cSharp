75ddded803b60aba76266cf46891d8ec5bf73501
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 14:38:37 -0300

    Exercicio 16

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio16 {
+     interface IDBBanco {
+        bool Salvar();
+        bool Atualizar();
+        bool Apagar();
+        bool Consultar();
+    }
+
+    class Teste : IDBBanco {
+        public bool Apagar() {
+            throw new NotImplementedException();
+        }
+
+        public bool Atualizar() {
+            throw new NotImplementedException();
+        }
+
+        public bool Consultar() {
+            throw new NotImplementedException();
+        }
+
+        public bool Salvar() {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Teste teste = new Teste();
+        }
+    }
+}
