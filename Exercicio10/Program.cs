f0178ecd90963bb46db2c379d9d3732022b3b9a5
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 13:34:40 -0300

    Exercicio 10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio10 {
+    class Program {
+        static void Main(string[] args) {
+            int n1 = 3;
+            int n2 = 4;
+            int n3 = 5;
+            if (n1 == n2) {
+                Console.WriteLine(" n1 e diferente de n2");
+            }
+            else {
+                Console.WriteLine(" n1 e igual a n2");
+            }
+            if (n3 < n2) {
+                Console.WriteLine(" n3 e menor n2");
+            }
+            else {
+                Console.WriteLine(" n3 nao e menor n2");
+            }
+        }
+    }
+}
