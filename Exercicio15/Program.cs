67542425f3fc1f27439f585610ef3225af3175d9
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 14:31:14 -0300

    Exercicio 15

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio15 {
+    class Program {
+        static void Main(string[] args) {
+            int z = 1;
+            string v = "33";
+
+            for (int i = 0; i > 3; i++) {
+                do {
+                    v = i.ToString() + z.ToString();
+                    z++;
+                } while (z < 2);
+            }
+
+            Console.WriteLine("v={0}", v);
+        }
+    }
+}
