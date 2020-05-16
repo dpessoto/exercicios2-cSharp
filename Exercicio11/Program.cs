a58ecfe61b1a5ba95e97e766a4801a7991eba210
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 13:41:03 -0300

    Exercicio 11

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio11 {
+    class Program {
+        static void Main(string[] args) {
+            string y = "";
+            char o1 = 'c';
+            switch (o1) {
+                case '1':
+                case 'c':
+                    y = "ACHEI A OPCAO 1"; goto case 'A';
+                    break;
+                case 'A':
+                    y = "ACHEI A OPCAO A";
+                    break;
+                default:
+                    y = "ACHEI A OPCAO 4";
+                    break;
+            }
+            Console.WriteLine("y={0}", y);
+        }
+    }
+}
