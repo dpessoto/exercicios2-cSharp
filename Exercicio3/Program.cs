0ff7800fddec4d1a689097ea3170001af2f1a5f5
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-16 10:45:36 -0300

    Exercicio 3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio3 {
+
+    public class Casa {
+        private string Cor;
+
+        public void SetCor(string nomeCor) {
+            this.Cor = nomeCor;
+        }
+
+        public string GetCor() {
+            return this.Cor;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            Casa casa = new Casa();
+
+            //casa.Cor = "Azul"; //não compila
+
+            casa.SetCor("Azul"); //compila
+
+            Console.WriteLine(casa.GetCor()); //compila
+
+        }
+    }
+}
